fe7f524031d7433f8e256ec1f5e14e6fd1816c95
raJiB <pokhrelrajib016@gmail.com>
2024-09-17 14:03:00 +0545

    added  user signout functionality and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React,{useState, useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import Auth from "./components/Auth.jsx"
 import Chat from './components/Chat.jsx'
 import Cookies from "universal-cookie"
+import { signOut } from 'firebase/auth'
+import {auth} from "./firebase-config.js"
 
 
 const cookies = new Cookies()
@@ -10,10 +12,15 @@ function App() {
   const [isAuth, setIsAuth] = useState(cookies.get('auth-token'));
   const [room, setRoom] = useState(null);
   console.log(room);
-  
+
+  const signUserOut= async ()=>{
+    await signOut(auth);
+    cookies.remove("auth-token");
+    setIsAuth(false)
+    setRoom("")
+  }
 
   const roomInputRef = useRef(null)
-
   if (!isAuth) {
     return (
       <div>
@@ -22,13 +29,20 @@ function App() {
     );
   }
 
-  return <div>{room ? <Chat room={room}/> :
-    <div>
-      <label>Enter room name: </label>
-      <input type="text"
-        ref={roomInputRef} />
-      <button onClick={()=>setRoom(roomInputRef.current.value)}>Enter Chat</button>
-    </div>}</div>
+  return (
+    <>
+      {room ?
+        <Chat room={room} /> :
+        <div>
+          <label>Enter room name: </label>
+          <input type="text"
+            ref={roomInputRef} />
+          <button onClick={() => setRoom(roomInputRef.current.value)}>Enter Chat</button>
+        </div>}
+        <div className="signout">
+          <button onClick={signUserOut}>signout</button>
+        </div>
+    </>
+  )
 }
-
 export default App
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,25 @@
-import React,{useState} from 'react'
-import { addDoc, collection, serverTimestamp } from "firebase/firestore"
+import React, { useEffect, useState } from 'react'
+import { addDoc, collection, serverTimestamp, onSnapshot, query, where, orderBy } from "firebase/firestore"
 import { auth, db } from "../firebase-config.js"
 
-function Chat({room}) {
+function Chat({ room }) {
 
   const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([])
   const messageRef = collection(db, "messages");
+
+  useEffect(() => {
+    const queryMessages = query(messageRef, where("room", "==", room), orderBy("createdAt"));
+    const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = []
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      })
+      setMessages(messages)
+    });
+
+    return () => unsubscribe();
+  }, [])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,12 +36,16 @@ function Chat({room}) {
 
   return (
     <div>
+      <div>{messages.map((message) =>
+        <h1 key={message.id}>{message.text}</h1>
+      )}
+      </div>
       <form action="" onSubmit={handleSubmit}>
         <input type="text"
           placeholder='Enter your message'
           value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)} 
-          />
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
         <button type='submit'>Send</button>
       </form>
     </div>
